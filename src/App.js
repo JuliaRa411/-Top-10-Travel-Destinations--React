8b63f97921728b178be012ac8252cb32309b9b26
JuliaRa411 <777.07j@gmail..com>
2022-02-01 17:45:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import { data } from './data';
+import logo from './logo.png';
+import './App.css';
+import circles from './circles.gif'
+ 
+ 
+ 
 function App() {
+  
+const [photos, setPhotos] = useState(data);
+const [t, setT] = useState(0);
+const [datas, setDatas] = useState(data);
+let newArr = [...datas];
+ 
+// remove object//
+const removeObject =  (id) => { 
+let newPhotos = photos.filter(photos =>photos.id !==id);
+setPhotos(newPhotos)
+}
+
+const[showMore, setShowMore] = useState(false) // Show more//
+  
+  const nextPhoto = (e, index) => {
+    setDatas(newArr);
+
+      const slide = document.querySelectorAll("#slide");
+      newArr[index] = photos[index];
+    
+      setT ((t => {
+        t ++ ;
+        if (t > 2 ) {
+        t = 0;
+        }
+        return t;
+        
+      }));
+      slide[index].setAttribute('src', newArr[index].image[t] );
+      return slide;
+    };
+
+    const prevPhoto = (e, index) => {
+      
+        const slide = document.querySelectorAll("#slide");
+        newArr[index] = photos[index];
+        setDatas(newArr);
+        setT ((t => {
+        t -- ;
+        if (t <0 ) {
+        t = 2;
+        }
+        return t;
+      }));
+
+      slide[index].setAttribute('src', newArr[index].image[t] );
+      return slide;
+    };
+
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container "> 
+       
+      <img src={ logo } className='App-logo' alt='logo'/>
+      <h1>Top {photos.length} Travel Destinations <br>
+      </br> to Visit  in the World </h1>
+     
+
+    {photos.map((element => {
+        const {id, destination, image, description} = element;
+        const index = photos.indexOf(element);
+
+        return (
+
+ 
+          <div className='item' key={ id } data-id={data.id}>
+            
+              <div className='App'>
+               <p className='number'>   { id  }   </p>
+               <img className='gif' src={circles} alt="loading..."  width="40px" /> 
+                <h3 className='destination' >  { destination } </h3>
+                 </div>
+                
+            <div className='App'>
+          <img id="slide"  src={ image[2] }  width = "400px"alt='nature'/>
+ 
+</div>
+<div className='App'>
+          {/* / / Show more// */}
+        
+          <p className='description' > {showMore ? description : description.substring (0,100) + "....."}
+     <button className='show' onClick={() => setShowMore(!showMore)}>{showMore? "Show less" : "Show more"} </button></p>
+</div>
+
+          <div className='btn'>
+            <button className='btnSlide' onClick={ (e)=> prevPhoto(e, index)  } id = { id } >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
+  <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+</svg>
+                </button>
+            <button className='btnSlide' onClick={ (e)=> nextPhoto(e, index)} id = { id } > 
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+  <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+</svg>  </button>
+ 
+ {/* // remove object// */}
+ 
+ <button className='btnSlide' onClick= {( ) => removeObject(id)}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></button>
+  
+          </div>
+          
+          </div>     
+         
+        )    
+    }))}
+
+ {/* //  Delete All // */}
+<div className='App'>
+    
+    <button className='delete' onClick={() =>setPhotos([])}> Delete All</button>
     </div>
-  );
+    </div>
+   
+    </div>
+    
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,108 @@
+ 
+import boraOne from './boraOne.jpg';
+import boraTwo from './boraTwo.jpg';
+import boraThree from './boraThree.jpg';
+import capoOne from './capoOne.jpg';
+import capoTwo from './capoTwo.jpg';
+import capoThree from './capoThree.jpg';
+import pariceOne from './pariceOne.jpg';
+import pariceTwo from './pariceTwo.jpg';
+import pariseThree from './pariseThree.jpg';
+import newOne from './newOne.jpg';
+import newTwo from './newTwo.jpg';
+import newThree from './newThree.jpg';
+import baliOne from './baliOne.jpg';
+import baliTwo from './baliTwo.jpg';
+import baliThree from './baliThree.jpg';
+import SwitzerlandOne from './SwitzerlandOne.jpg';
+import SwitzerlandTwo from './SwitzerlandTwo.jpg';
+import SwitzerlandThree  from './SwitzerlandThree.jpg';
+import londonOne from './londonOne.jpg';
+import londonTwo from './londonTwo.jpg';
+import londonThree from './londonThree.jpg';
+import niagaraOne from './niagaraOne.jpg';
+import niagaraTwo from './niagaraTwo.jpg';
+import niagaraThree from './niagaraThree.jpg';
+import phuketOne from './phuketOne.jpg';
+import phuketTwo from './phuketTwo.jpg';
+import phuketThree from './phuketThree.jpg';
+import maldivesOne from './maldivesOne.jpg';
+import maldivesTwo from './maldivesTwo.jpg' 
+import maldivesThree from './maldivesThree.jpg';
+ 
+ 
+export const data  =[
+
+{
+    id: 1,
+    destination: "Bora Bora Island, French Polynesia",
+   image: [boraOne,boraTwo,boraThree], 
+    description:"The epitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxurious stays and adventurous activities." ,
+},
+
+{
+    id: 2,
+    destination: "Cappadocia, Turkey – Most Magical Place on Earth",
+    image: [capoOne,capoTwo,capoThree],
+    description: "Magic can only be seen and seeing is believing. This amazing place in Turkey offers a breathtaking view that words would fall short when we begin to explain it. Coming to Cappadocia’s you realize that everything done by nature has its own beauty.",
+},
+
+{
+    id: 3,
+    destination:"Paris, France – Most Visited City in the World",
+    image:  [pariseThree, pariceTwo,  pariceOne, ],
+    description: "A whopping 89.4 million tourists visiting France each year is not in vain. It certainly has a reason and Paris is perhaps one BIG reason.  It’s almost impossible to not fall in love with this city. It is full of history, beauty, and love." ,
+},
+
+{
+    id: 4,  
+    destination: " New York, United States – The city which Never Sleeps",
+    image: [newOne, newTwo, newThree],
+    description: "New York is one city that is bestowed by its designers with an infrastructure to please one and all. No traveler has been disappointed ever while visiting New York. Here you can feel the synergy between urban and natural areas and how the greens have been retained irrespective of major developments in the city.",
+},
+
+{
+    id: 5,
+    destination: "Bali, Indonesia",
+    image: [ baliOne, baliTwo,  baliThree],
+    description: "Bali is an Indonesian Island and one of the best holiday destinations in the Indonesian archipelago. Bali is known for its volcanic mountains, history, art & culture, food, temples and beautiful sandy beaches.",
+},
+
+{
+    id:6,
+    destination: "Blue Lake Luzern, Switzerland",
+    image: [SwitzerlandOne, SwitzerlandTwo, SwitzerlandThree ],
+    description: "Every year more than 9 million tourists witness this beautiful place. It is a paradise for newlyweds and you will often observe couples there. The blue waters in the city and the greens together synergize into making this one of the most visited places in Switzerland. Lucerne is famous for its postcard views and lovely weather.",
+},
+
+
+{
+    id:7,
+    destination: "London, United Kingdom",
+    image: [londonOne, londonTwo, londonThree],
+    description: "London is one of the biggest and most populated cities in the world and that makes it the best place to visit in the world. London is a fashion hub and cultural capital. Visitors come here to enjoy its royalty, pubs, theater, museum and the amazing landscape it offers.",
+},
+
+{
+    id:8,
+    destination: "Niagara Falls, Canada/USA",
+    image: [niagaraOne,niagaraTwo, niagaraThree],
+    description: "Niagara Falls is one of the highest waterfalls in the world. It is a collection of three waterfall which is located on the US-Canada border along with the Niagara River. Niagara Falls is famous for its majestic height (51 Meters) and shocking flow rate (225,000 Cubic feet per second).",
+},
+
+{
+    id:9,
+    destination: "Phuket Thailand:",
+    image: [phuketOne,phuketTwo,phuketThree], 
+    description:"Phuket is the largest island in Thailand and rated as one of the best beaches in the world. The white sand, blue water, spicy Thai food, palm tree, lively town, adventurous sports and sandy beaches make it the best holiday destination.",
+},
+
+  
+{
+    id:10,
+    destination: "Maldives",
+    image: [maldivesOne, maldivesTwo, maldivesThree], 
+    description:"Maldives is a South Asian Island Country. It is one of the most popular tourist attractions in the world or you can say paradise on earth. The interesting thing about the Maldives is that the first underwater cabinet meeting was held here. The actual beauty of this place is underwater with colorful coral reefs, exotic fishes and vibrant marine life.",
+},
+ 
+]
